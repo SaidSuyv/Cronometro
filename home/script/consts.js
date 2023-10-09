4811b615e12ebc1e8f21f09381de5a6557ba4116
SaidSuyv <sirsuyvi@gmail.com>
2023-10-09 05:08:05 -0500

    update

---
+++ home/script/consts.js
@@ -0,0 +1,8 @@
+const TIMER = {
+    name: "Timer",
+    interval: null,
+    date: document.querySelector("section#timer p.date"),
+    time: document.querySelector("section#timer p.time")
+}
+
+const TITLE_FRAME = document.querySelector("p#current_frame");
